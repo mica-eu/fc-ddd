7f818edbf90988f2fd617e1d79a7d0365f131a66
Micael Lima <5789811+mica-eu@users.noreply.github.com>
2023-07-11 19:30:42 -0300

    feat: Product

--- src/entity/product.spec.ts
+++ src/entity/product.spec.ts
@@ -1,50 +1,5 @@
 import { randomUUID } from 'crypto';
-
-class Product {
-  #id: string;
-  #name: string;
-  #price: number;
-
-  constructor(id: string, name: string, price: number) {
-    this.#id = id;
-    this.#name = name;
-    this.#price = price;
-    this.validate();
-  }
-
-  get name(): string {
-    return this.#name;
-  }
-
-  get price(): number {
-    return this.#price;
-  }
-
-  changeName(name: string): void {
-    this.#name = name;
-    this.validate();
-  }
-
-  changePrice(price: number): void {
-    this.#price = price;
-    this.validate();
-  }
-
-  private validate(): void {
-    if (!this.#id.trim()) {
-      throw new Error('Missing required param <id>');
-    }
-    if (!this.#name.trim()) {
-      throw new Error('Missing required param <name>');
-    }
-    if (this.#price === undefined || this.#price === null) {
-      throw new Error('Missing required param <price>');
-    }
-    if (this.#price < 0) {
-      throw new Error('<price> param must be greater than 0');
-    }
-  }
-}
+import { Product } from './product';
 
 describe('Product', () => {
   it('throws error when id is empty', () => {
